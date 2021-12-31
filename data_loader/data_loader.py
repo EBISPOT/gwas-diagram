8f2cdd8de532460b5b88463bfaff0b2dc10d90fc
Daniel Suveges <daniel.suveges@protonmail.com>
2021-12-31 00:59:22 +0000

    Updates on class, integration into script.

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -21,39 +21,40 @@ class DataLoader:
 
         # Load the association data if the file exists:
         try:
-            logging.info('Pooled data was found ({}). Loading...'.format(self.GWAS_DATA_PICKLE_FILE))
+            logging.info('Trying to load dataset ({})....'.format(self.GWAS_DATA_PICKLE_FILE))
             pooled_terms = pickle.load(open(self.GWAS_DATA_PICKLE_FILE, "rb"))
             self.pooled_terms = pooled_terms
 
         except FileNotFoundError:
 
             # Downloading and filtering data:
+            logging.warning('Pre-processed data was not found. Preparing now...')
+
             logging.info(f'Downloading parent file: {self.parent_mapping_file}.')
-            self.parent_mapping_df = self.get_parent_dataframe()
+            self.parent_mapping_df = self.__get_parent_dataframe()
 
             logging.info(f'Downloading association file: {self.association_file}.')
-            self.association_df = self.get_association_dataframe()
+            self.association_df = self.__get_association_dataframe()
+            logging.info(f'Number of association: {len(self.association_df)}')
 
             logging.info(f'Downloading ancestry file: {self.ancestry_file}.')
-            self.ancestry_df = self.get_ancestry_dataframe()
+            self.ancestry_df = self.__get_ancestry_dataframe()
 
             # Pool associations with parent terms:
             logging.info('Joining data...')
-            self.pooled_terms = self.pool_dataframes()
+            self.pooled_terms = self.__pool_dataframes()
 
             # saving data into a pickled file:
-            logging.info('Saving pickle file.')
-            pickle.dump(pooled_terms, open(self.pooled_data_file, "wb"))    
+            logging.info(f'Saving pickle file: {self.GWAS_DATA_PICKLE_FILE}.')
+            pickle.dump(pooled_terms, open(self.pooled_data_file, "wb"))
 
     def get_data(self) -> pd.DataFrame:
-        """Returns the association as a dataframe."""
-        return self.association_df
+        """Returns the pooled association as a dataframe."""
+        return self.pooled_terms
 
-    def get_parent_dataframe(self) -> pd.DataFrame:
+    def __get_parent_dataframe(self) -> pd.DataFrame:
         """Getting a mapping of EFO terms to their parent terms.
 
-        Args:
-            parent_mapping_file (str): Path to the file containing the parent mapping.
         Returns:
             pd.DataFrame: A dataframe containing the parent mapping.
         """
@@ -62,7 +63,7 @@ class DataLoader:
         columns_to_extract = ['EFO URI', 'Parent term']
 
         # Downloading EFO mapping file:
-        parent_mapping_df = pd.read_csv(parent_mapping_file, sep='\t')
+        parent_mapping_df = pd.read_csv(self.parent_mapping_file, sep='\t')
 
         try:
             parent_mapping_df = parent_mapping_df[columns_to_extract]
@@ -70,41 +71,37 @@ class DataLoader:
             raise('[Error] The parent mapping data frame does not have some of the required columns ({}).'.format(
                 ','.join(columns_to_extract)))
 
-        # Filter out onlyt the required fields:
+        # Filter out only the required fields:
         parent_mapping_df = parent_mapping_df[['EFO URI', 'Parent term']].drop_duplicates()
 
         # Rename columns:
-        parent_mapping_df.rename(columns={'EFO URI': 'EFO_URI',
-                                        'Parent term': 'EFO_PARENT'})
+        parent_mapping_df.rename(columns={'EFO URI': 'EFO_URI', 'Parent term': 'EFO_PARENT'}, inplace=True)
 
         return parent_mapping_df
 
-
-    def get_association_dataframe(self) -> pd.DataFrame:
+    def __get_association_dataframe(self) -> pd.DataFrame:
         """ Fetching and formatting association data.
 
-        Args:
-            association_file (str): Path to the file containing the association data.
         Returns:
             pd.DataFrame: A dataframe containing the association data.
         """
 
         # Downloading associations:
-        association_df = pd.read_csv(association_file, sep='\t', dtype=str)
+        association_df = pd.read_csv(self.association_file, sep='\t', dtype=str)
 
         # Get a list of columns we want to filter the data in the future:
-        columns_to_extract = ['DATE ADDED TO CATALOG', 'PUBMEDID',
-                            'REGION', 'CHR_ID', 'DISEASE/TRAIT',
-                            'MAPPED_GENE', 'SNP_GENE_IDS',
-                            'SNPS', 'CHR_POS', 'MAPPED_TRAIT',
-                            'CONTEXT', 'INTERGENIC', 'INITIAL SAMPLE SIZE',
-                            'REPLICATION SAMPLE SIZE',
-                            'PVALUE_MLOG', 'MAPPED_TRAIT_URI', 'STUDY ACCESSION']
+        columns_to_extract = [
+            'DATE ADDED TO CATALOG', 'PUBMEDID',
+            'REGION', 'CHR_ID', 'DISEASE/TRAIT',
+            'MAPPED_GENE', 'SNP_GENE_IDS',
+            'SNPS', 'CHR_POS', 'MAPPED_TRAIT',
+            'CONTEXT', 'INTERGENIC', 'INITIAL SAMPLE SIZE',
+            'REPLICATION SAMPLE SIZE',
+            'PVALUE_MLOG', 'MAPPED_TRAIT_URI', 'STUDY ACCESSION'
+        ]
 
         # Filtering columns:
         association_df = association_df[columns_to_extract]
-
-        # Filtering rows criteria:
 
         # Exclude associations without genomic mapping:
         association_df = association_df.loc[~association_df.REGION.isna()]
@@ -116,7 +113,7 @@ class DataLoader:
         association_df.PVALUE_MLOG = association_df.PVALUE_MLOG.astype('float')
         association_df = association_df.loc[association_df.PVALUE_MLOG > -np.log10(5e-8)]
 
-        # Exclude association with multiple snps: <- this criteria might be too harsh
+        # Exclude association with multiple snps:
         association_df = association_df.loc[~association_df.CHR_POS.str.contains(';')]
         association_df = association_df.loc[~association_df.CHR_POS.str.contains('x')]
 
@@ -139,8 +136,7 @@ class DataLoader:
 
         return association_df
 
-
-    def get_ancestry_dataframe(self) -> pd.DataFrame:
+    def __get_ancestry_dataframe(self) -> pd.DataFrame:
         """ Fetching and formatting ancestry data.
 
         Args:
@@ -153,20 +149,30 @@ class DataLoader:
         ancestry_columns = ['STUDY ACCESSION', 'BROAD ANCESTRAL CATEGORY']
 
         # Download ancestry data:
-        ancestry_df = pd.read_csv(ancestry_file, sep="\t", index_col=False, usecols=ancestry_columns)
+        ancestry_df = pd.read_csv(self.ancestry_file, sep="\t", index_col=False, usecols=ancestry_columns)
 
         return ancestry_df
 
+    def __pool_dataframes(self) -> pd.DataFrame:
+        """Joining association dataframe with the EFO parent mappings and ancestry.
 
-    def pool_dataframes(self) -> pd.DataFrame:
+        Returns:
+            pd.DataFrame: A dataframe containing the pooled data.
+        """
 
-        # Adding parent term to all rows:
-        pooled_df = association_df.merge(parent_mapping_df, how='left', left_on='MAPPED_TRAIT_URI', right_on='EFO_URI')
+        pooled_df = (
+            self.association_df
 
-        # Adding ancestry df too:
-        pooled_df = pooled_df.merge(ancestry_df, how='left', left_on='STUDY ACCESSION', right_on='STUDY ACCESSION')
+            # Adding parent term to all rows:
+            .merge(self.parent_mapping_df, how='left', left_on='MAPPED_TRAIT_URI', right_on='EFO_URI')
+
+            # Adding ancestry df too:
+            .merge(self.ancestry_df, how='left', left_on='STUDY ACCESSION', right_on='STUDY ACCESSION')
+        )
 
         # Remove lines without parent mapping: <- there should be report on this...
         pooled_df = pooled_df.loc[~pooled_df['EFO_PARENT'].isna()]
         return pooled_df
 
+    def __len__(self):
+        return len(self.pooled_terms)
